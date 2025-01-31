014e4c4c875be6d9cd3794291f9e1331c3b9cab0
dev-mohit-antino <mohit.antino@gmail.com>
2025-01-31 11:39:12 +0530

    Initial commit

---
+++ lib/petsList/pets_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoriesListScreen extends StatefulWidget {
+  const CategoriesListScreen({super.key});
+
+  @override
+  State<CategoriesListScreen> createState() => _CategoriesListScreenState();
+}
+
+class _CategoriesListScreenState extends State<CategoriesListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
